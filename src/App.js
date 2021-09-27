c87445568dc97e6164b35b2b0e61f9fa74566ebe
Mario <mmdw1976@gmail.com>
2021-09-27 16:17:01 +0200

    added script for google services in index.html

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React from 'react';
 
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-const PageOne = () => {
-    return ( 
-        <div>pageOne</div>
-     );
-}
-
-const PageTwo = () => {
-    return ( 
-        <div>pageTwo</div>
-     );
-}
- 
-
+import Header from './components/Header';
+import StreamCreate from './components/streams/StreamCreate';
+import StreamDelete from './components/streams/StreamDelete';
+import StreamEdit from './components/streams/StreamEdit';
+import StreamList from './components/streams/StreamList';
+import StreamShow from './components/streams/StreamShow';
 
 const App = () => {
-    return ( 
+  return (
+    <div className='ui container'>
+      <BrowserRouter>
         <div>
-            <BrowserRouter>
-                <Route path="/" exact component={PageOne} />
-                <Route path="/page-two" component={PageTwo} />
-            </BrowserRouter>
+          <Header />
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' exact component={StreamCreate} />
+          <Route path='/streams/edit' exact component={StreamEdit} />
+          <Route path='/streams/delete' exact component={StreamDelete} />
+          <Route path='/streams/show' exact component={StreamShow} />
         </div>
-     );
-}
- 
+      </BrowserRouter>
+    </div>
+  );
+};
+
 export default App;
